62174dbbb45007276b43453731863f15c5742b40
reycota <rey.cota@gmail.com>
2024-07-04 14:13:47 -0700

    I added the third portion of the function, to return the choice at the random index.

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ function getComputerChoice(){
     const choices = ["Rock", "Paper", "Scissors"];
     //step 2: generate a random index
     const randomIndex = Math.floor(Math.random() * choices.length);
+    //step 3: return the choice at the random index
+    return choices[randomIndex];
 }
+
 console.log();

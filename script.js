19e6a6fd5c0eb66e0881ef4f483043fe6e2efe2c
reycota <rey.cota@gmail.com>
2024-07-04 14:12:10 -0700

    added the second portion of the function, to generate a random index sing the math random method.

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getComputerChoice(){
     //step 1: define the possible choices
     const choices = ["Rock", "Paper", "Scissors"];
+    //step 2: generate a random index
+    const randomIndex = Math.floor(Math.random() * choices.length);
 }
+console.log();

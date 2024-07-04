0316f3fb0851ce2e303c7f64444f5f3d52b80258
reycota <rey.cota@gmail.com>
2024-07-04 15:39:00 -0700

    created a function getHumanChoice, where the human is prompted to select either rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function getComputerChoice(){
     //step 1: define the possible choices
     const choices = ["Rock", "Paper", "Scissors"];
+
     //step 2: generate a random index
     const randomIndex = Math.floor(Math.random() * choices.length);
+
     //step 3: return the choice at the random index
     return choices[randomIndex];
 }
 
-console.log();
+function getHumanChoice(){
+    //step 1: prompt the user to enter their choice
+    const choice = prompt("Please select your choice (Rock, Paper, or Scissors):");
+
+    //step 2:
+}
+console.log(getComputerChoice());
